b1d092fa531ca51ba8a84f2088d5a93813240697
GianTechWizard <giannkolebaa@gmail.com>
2025-11-29 23:01:15 +0700

    New

--- Backend/public/my_registrasions.php
+++ Backend/public/my_registrasions.php
@@ -4,8 +4,26 @@ require_once "../includes/db_connect.php";
 require_once "../includes/session_check.php"; 
 
 // pengecekkan session_check untuk memastikan user harus login dulu (ISI CODE SINI- MARCEL)
-
+    $id_user = $_SESSION['id_user'];
 
 // proses Pengambilan semua event yang sudah didaftarkan oleh user (ISI CODE SINI- MARCEL)
+    $sql = "SELECT p.*, e.judul_event FROM pendaftaran_event p
+        JOIN events e ON p.id_event = e.id_event
+        WHERE id_user = ?";
 
+// Tanda "?" adalah placeholder yang nanti diganti dengan id_user dari session
+
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id_user);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    exit;
 ?>
--- Backend/public/profile.php
+++ Backend/public/profile.php
@@ -16,35 +16,30 @@
 // ====================================================================
 
 
+// Marcell
+
+
 // Menghubungkan ke database (ISI SINI - MARCEL)
+    require_once "../includes/db_connection.php";
+    require_once "../includes/session_check.php"; 
 
 // Memanggil fungsi (ISI SINI - MARCEL)
+    require_once "../includes/functions.php";
 
 // Mengambil ID user dari session (ISI SINI - MARCEL)
+    $id_user = $_SESSION['id_user'];
 
 // Query data user (ISI SINI - MARCEL)
+    $sql = "SELECT id_user, username, email, no_hp FROM users WHERE id_user = ?";
+    $stmt = mysqli_prepare($conn, $sql);
 
+    mysqli_stmt_bind_param($stmt, "i", $id_user);
+    mysqli_stmt_execute($stmt);
 
+    $result = mysqli_stmt_get_result($stmt);
+    $userData = mysqli_fetch_assoc($result);
 
-
-
-
-
-
-
-
-// --------------------------------------------------------------
-// Bagian FRONTEND
-// Buat UI form profil menggunakan HTML, contoh:
-//
-// <form action="../process/profile_update_process.php" method="POST">
-//     <input type="text" name="nama" value="<?= $user['nama_lengkap'] ? >">
-//     <input type="text" name="no_hp" value="<?= $user['no_hp'] ? >">
-//     <input type="password" name="password">
-//     <button type="submit">Update</button>
-// </form>
-//
-// Backend tidak membuat UI.
-// Backend hanya menyediakan $user untuk ditampilkan.
-// --------------------------------------------------------------
+    header("Content-Type: application/json");
+    echo json_encode($userData);
+    exit;
 ?>
